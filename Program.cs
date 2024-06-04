c351d4b1bee71196ec5f6a547b936cd70f71b509
fuhrdan <165685619+fuhrdan@users.noreply.github.com>
2024-06-04 16:51:28 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿int longestPalindrome(char* s) {
+    int len = strlen(s);
+    if(len <= 1) return len;
+    int palLength[2001] = {0};
+    int convert = 0;
+    int retLen = 0;
+    for(int x = 0; x < len; x++)
+    {
+        convert = s[x];
+        palLength[convert]++;
+//        printf("retLen = %d || %d\n",convert,palLength[convert]);
+        if(palLength[convert] >= 2)
+        {
+            retLen = retLen + 2;
+//            printf("Return = %d\n",retLen);
+            palLength[convert] = 0;
+        }
+
+    }
+    if(retLen < len)
+    {
+        retLen = retLen + 1;
+    }
+    return retLen;
+}
